945b1310320cb81a0babb9181b70d3db5c39ab96
beastrc <makarov0114@outlook.com>
2017-04-09 19:06:55 +0200

    DEL-871: Showcase content links, multiple choice and URL slugs

--- src/Components/RichTextElement.js
+++ src/Components/RichTextElement.js
@@ -5,18 +5,22 @@ import { resolveContentLink } from '../Utilities/ContentLinks';
 function handleClick(element, router, e) {
   if (e.target.tagName === "A" && e.target.hasAttribute("data-item-id")) {
     e.preventDefault();
+
     const id = e.target.getAttribute("data-item-id");
     const link = element.links[id];
 
     if (link) {
-      router.push(resolveContentLink(link));
+      const path = resolveContentLink(link);
+      if (path) {
+        router.push(path);
+      }
     }
   }
 }
 
 const RichTextElement = (props) => {
   return (
-    <div className={props.className} dangerouslySetInnerHTML={{ __html: props.element.value }} onClick={(e) => handleClick(props.element, props.router, e)}/>
+    <div className={props.className} dangerouslySetInnerHTML={{ __html: props.element.value }} onClick={(e) => handleClick(props.element, props.router, e)} />
   );
 };
 
