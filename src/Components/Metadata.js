3bafffb35dc7e930ca602d1b72cfdda520a50b3a
beastrc <makarov0114@outlook.com>
2019-03-05 13:00:35 +0100

    Update README.md

--- src/Components/Metadata.js
+++ src/Components/Metadata.js
@@ -1,84 +1,54 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
-import _ from 'lodash';
 
 const Metadata = props => {
   return (
     <Helmet>
-      {!_.has(props, 'title.value') || props.title.value.trim().length === 0 ? (
+      {!props.title ? (
         <title>Dancing Goat</title>
       ) : (
         <title>{props.title.value.trim()}</title>
       )}
-
-      {!_.has(props, 'description.value') ||
-      props.description.value.trim().length === 0 ? null : (
+      {!props.description ? null : (
         <meta name="description" content={props.description.value.trim()} />
       )}
 
-      {!_.has(props, 'ogTitle.value') || props.ogTitle.value.trim().length === 0
-        ? null
-        : [
-            <meta
-              key="og:title"
-              property="og:title"
-              content={props.ogTitle.value.trim()}
-            />,
-            <meta key="og:type" property="og:type" content="website" />,
-            <meta
-              key="og:url"
-              property="og:url"
-              content={window.location.href}
-            />
-          ]}
-
-      {!_.has(props, 'ogImage.value[0].url') ? null : (
+      {!props.ogTitle ? null : (
+        <meta property="og:title" content={props.ogTitle.value.trim()} />
+      )}
+      {!props.ogTitle ? null : <meta property="og:type" content="website" />}
+      {!props.ogTitle ? null : (
+        <meta property="og:url" content={window.location.href} />
+      )}
+      {!props.ogImage ? null : (
         <meta property="og:image" content={props.ogImage.value[0].url} />
       )}
-
-      {!_.has(props, 'ogDescription.value') ||
-      props.ogDescription.value.trim().length === 1 ? null : (
+      {!props.ogDescription ? null : (
         <meta property="og:description" content={props.ogDescription.value} />
       )}
 
-      {!_.has(props, 'twitterTitle.value') ||
-      props.twitterTitle.value.trim().length === 0
-        ? null
-        : [
-            <meta
-              key="twitter:title"
-              name="twitter:title"
-              content={props.twitterTitle.value.trim()}
-            />,
-            <meta
-              key="twitter:card"
-              name="twitter:card"
-              content="summary_large_image"
-            />
-          ]}
-
-      {!_.has(props, 'twitterSite.value') ||
-      props.twitterSite.value.trim().length === 0 ? null : (
+      {!props.twitterTitle ? null : (
+        <meta name="twitter:title" content={props.twitterTitle.value.trim()} />
+      )}
+      {!props.twitterTitle ? null : (
+        <meta name="twitter:card" content="summary_large_image" />
+      )}
+      {!props.twitterSite ? null : (
         <meta name="twitter:site" content={props.twitterSite.value.trim()} />
       )}
-
-      {!_.has(props, 'twitterCreator.value') ||
-      props.twitterCreator.value.trim().length === 0 ? null : (
+      {!props.twitterCreator ? null : (
         <meta
           name="twitter:creator"
           content={props.twitterCreator.value.trim()}
         />
       )}
-
-      {!_.has(props, 'twitterDescription.value') ||
-      props.twitterDescription.value.trim().length === 0 ? null : (
+      {!props.twitterDescription ? null : (
         <meta
-          name="twitter:description.value"
+          name="twitter:description"
           content={props.twitterDescription.value.trim()}
         />
       )}
-
-      {!_.has(props, 'twitterImage.value[0].url') ? null : (
+      {!props.twitterImage ? null : (
         <meta name="twitter:image" content={props.twitterImage.value[0].url} />
       )}
     </Helmet>
